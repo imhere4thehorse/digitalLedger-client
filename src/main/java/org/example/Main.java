dd0ef0b167bd8741922ded2f8237d3c090639313
imhere4thehorse <106560789+imhere4thehorse@users.noreply.github.com>
2023-02-16 10:48:25 -0700

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,12 @@
-package org.example;public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Window thisGUI = new Window();
+//        try {
+//            ServerRequest requestIt = new ServerRequest();
+//        } catch (IOException e){
+//            System.out.println("Port already in use, try another one.");
+//        }
     }
 }
